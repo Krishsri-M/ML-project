11a9285b2622cd8421ea484df6fcc5e2f165af76
Krishsri-M <shrimega8778@gmail.com>
2026-04-16 10:56:58 +0530

    adding features

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def health():
     return jsonify({"status": "ok"})
 
 if __name__== "__main__":
-    app.run(debug=True, host="0.0.0.0",  port=5000) # Host: 0.0.0.0 means any computer can  connect to this host 
+    app.run(debug=True, host="0.0.0.0",  port=8000) # Host: 0.0.0.0 means any computer can  connect to this host 
